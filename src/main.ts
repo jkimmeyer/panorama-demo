35bd0a1ac8ae100a606d0109bdc44e8edbaf2fb1
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2023-09-24 20:31:49 +0100

    Adapt demo page

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,91 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+<div class="page">
+  <header class="page-header">
+    <h1>LOGO</h1>
+    <nav class="page-navigation">
+    <ul>
+      <li><a href="#">Overview</a></li>
+      <li><a href="#">Details</a></li>
+      <li><a href="#">Highlights</a></li>
+      <li><a href="#">Sign Up</a></li>
+      <li><a href="#">Log In</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main class="page-main">
+    <img src="https://placehold.co/1920x400" style="width: 100%;"/>
+    <section>
+      <form>
+        <div class="form-input">
+          <label for="#input">E-Mail</label>
+          <input id="input" type="email">
+        </div>
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+        <fieldset>
+          <legend>Interval</legend>
+          <div class="form-radio">
+            <input id="newsletter-interval-daily" type="radio" name="newsletter-interval" value="daily">
+            <label for="newsletter-interval-daily">Daily</label>
+          </div>
+          <div class="form-radio">
+            <input id="newsletter-interval-weekly" type="radio" name="newsletter-interval" value="weekly">
+            <label for="newsletter-interval-weekly">Weekly</label>
+          </div>
+          <div class="form-radio">
+            <input id="newsletter-interval-monthly" type="radio" name="newsletter-interval" value="monthly">
+            <label for="newsletter-interval-monthly">Monthly</label>
+          </div>
+        </fieldset>
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+        <div class="form-checkbox">
+          <input id="newsletter-terms" type="checkbox" name="newsletter-terms">
+          <label for="newsletter-terms">I accept the terms and conditions</label>
+        </div>
+
+        <button>
+          Register
+        </button>
+        <button>
+          Submit
+        </button>
+      </form>
+
+      <div class="card">
+        <a href="#">
+          <img src="https://placehold.co/400x300" />
+        </a>
+
+        <h2 class="card-title"><strong>Card Text</strong> Brand Information</h2>
+
+        <div class="card-subtitle">19,99 €</div>
+
+        <p>Lorem ipsum dolor sit amet.  Lorem ipsum dolor sit amet.  Lorem ipsum dolor sit amet.  Lorem ipsum dolor sit amet. </p>
+      </div>
+
+      <div class="card">
+        <a href="#">
+          <img src="https://placehold.co/400x300" />
+        </a>
+
+        <h2 class="card-title"><strong>Card Text</strong> Brand Information</h2>
+
+        <div class="card-subtitle">19,99 €</div>
+
+        <p>Lorem ipsum dolor sit amet.  Lorem ipsum dolor sit amet.  Lorem ipsum dolor sit amet.  Lorem ipsum dolor sit amet. </p>
+      </div>
+
+      <div class="card">
+        <a href="#">
+          <img src="https://placehold.co/400x300" />
+        </a>
+
+        <h2 class="card-title"><strong>Card Text</strong> Brand Information</h2>
+
+        <div class="card-subtitle">19,99 €</div>
+
+        <p>Lorem ipsum dolor sit amet.  Lorem ipsum dolor sit amet.  Lorem ipsum dolor sit amet.  Lorem ipsum dolor sit amet. </p>
+      </div>
+    </section>
+  </main>
+</div>
+`;
